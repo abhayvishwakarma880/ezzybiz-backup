04dd1922c14148b26398d5c752530e31e3e9ccb9
Abhay Vishwakarma <hb9868313@gmail.com>
2026-05-25 10:29:18 +0530

    Done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -34,7 +34,7 @@ const CONTACT_INFO = [
   {
     icon: MapPin,
     title: "Office Address",
-    description: "Office 210, Business Bay,\nDubai, United Arab Emirates",
+    description: "1603 The Citadel Tower,\n Business Bay, Dubai",
   },
   {
     icon: Phone,
@@ -584,7 +584,7 @@ export default function Contact() {
 
             <div className="rounded-2xl overflow-hidden shadow-md border border-gray-100">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.5690058643872!2d55.25831347447071!3d25.184026777719485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69d047294591%3A0x9ad7e353822dc797!2sThe%20Citadel%20Tower!5e0!3m2!1sen!2sin!4v1779639578446!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.5690058643872!2d55.25831347447071!3d25.184026777719485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69d047294591%3A0x9ad7e353822dc797!2sThe%20Citadel%20Tower!5e0!3m2!1sen!2sin!4v1779685105768!5m2!1sen!2sin"
                 width="100%"
                 height="350"
                 style={{ border: 0 }}
